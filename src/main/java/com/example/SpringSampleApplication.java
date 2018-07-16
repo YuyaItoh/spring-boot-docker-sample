1d2e29998faf96fcbbb6f500c5ec0dd9c481518b
YuyaItoh <yuya.itoh.from2012@gmail.com>
2018-07-16 12:16:37 +0900

    コンパイルエラーを解消し、テストケースが1つもない場合

--- src/main/java/com/example/SpringSampleApplication.java
+++ src/main/java/com/example/SpringSampleApplication.java
@@ -1,12 +1,13 @@
 package com.example;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringSampleApplication implements CommandLineRunner {
     public static void main(String[] args) {
-        SpringApplicatio.run(SpringSampleApplication.class, args);
+        SpringApplication.run(SpringSampleApplication.class, args);
     }
 
     @Override
