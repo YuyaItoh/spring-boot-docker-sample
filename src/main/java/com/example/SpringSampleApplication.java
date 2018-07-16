3268cac0702dac4011c4c8ddf66afa4647bab6c9
YuyaItoh <yuya.itoh.from2012@gmail.com>
2018-07-16 12:14:21 +0900

    コンパイルエラーが出るようにする

--- src/main/java/com/example/SpringSampleApplication.java
+++ src/main/java/com/example/SpringSampleApplication.java
@@ -1,13 +1,12 @@
 package com.example;
 
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringSampleApplication implements CommandLineRunner {
     public static void main(String[] args) {
-        SpringApplication.run(SpringSampleApplication.class, args);
+        SpringApplicatio.run(SpringSampleApplication.class, args);
     }
 
     @Override
